8974e1d1b8fb07fd98d78ecbcd756c66a572eeeb
TODO <91803877+WassimBek@users.noreply.github.com>
2023-04-19 18:12:49 +0200

    solving error problem

--- app.js
+++ app.js
@@ -15,14 +15,20 @@ Clk.addEventListener('click',()=>{
         Programme() ;
 }) ;
 //inputs is one of three inputs and required is one of three text of required
-const Checking_empty =(inputs , requireds)=>{
+const Checking_empty =(inputs , requireds , Must)=>{
     let value1 = inputs.value;
+    
     if (value1.length === 0) {
         requireds.setAttribute('style' , 'visibility : visible ');
         inputs.style.borderColor = "red" ;
+        Must.style.visibility = "hidden" ;
         return false ;
-    }
+    }else {
+        requireds.setAttribute('style' , 'visibility : hidden ');
+        inputs.style.borderColor = "none" ;
+        Must.style.visibility = "hidden" ;
     return true ;
+    }
 }
 const Programme = () => {
  let Bool_D = Days() ;
@@ -46,9 +52,11 @@ const Programme = () => {
 }
 const Days = () => {
     let Day_input = D_input.value ;
-    let bool_checkD = Checking_empty(D_input,D_Required) ;
+    let bool_checkD = Checking_empty(D_input,D_Required,D_Must) ;
     if(bool_checkD == true){
         if(Number(Day_input) >= 1 && Number(Day_input) <= 31){
+            D_Must.style.visibility="hidden" ;
+            D_input.style.borderColor = "none" 
             return true ;
         } else {
             D_Must.style.visibility="visible" ;
@@ -62,13 +70,19 @@ const Days = () => {
 const Month = () => {
     let Month_input = M_input.value ;
     let input_d = D_input.value ;
-    let bool_checkM  = Checking_empty(M_input,M_Required) ;
+    let bool_checkM  = Checking_empty(M_input,M_Required,M_Must) ;
  if(bool_checkM){
     if (Number(Month_input) >= 1 && Number(Month_input)<=12 ){
         if(Number(Month_input) % 2 === 0 && Number(input_d) <= 30) {
             if(Number(Month_input) === 2) {
                 if(Number(input_d) <= 29) 
-                return true ;
+                {
+                  D_Must.style.visibility = "hidden" ;
+                  D_input.style.borderColor = "none" ;
+                  M_Must.style.visibility = "hidden" ;
+                  M_input.style.borderColor = "none" ;
+                  return true ;  
+                }
                 else {
                 D_Must.style.visibility = "visible" ;
                 D_input.style.borderColor = "red" ;
@@ -76,10 +90,14 @@ const Month = () => {
             }else 
             return true ;
         } else if(Number(Month_input) % 2 != 0 && Number(input_d)<=31) {
+                  D_Must.style.visibility = "hidden" ;
+                  D_input.style.borderColor = "none" ;
+                  M_Must.style.visibility = "hidden" ;
+                  M_input.style.borderColor = "none" ;
             return true ;
         }else {
-            D_Must.style.visibility = "visible" ;
-            D_input.style.borderColor = "red" ;
+            M_Must.style.visibility = "visible" ;
+            M_input.style.borderColor = "red" ;
         }
     } else 
     {
@@ -92,10 +110,14 @@ const Month = () => {
 const Year = () => {
     let Year_input = Y_input.value; 
     let DateNow = new Date() ;
-    let bool_checkY = Checking_empty(Y_input,Y_Required) ;
+    let bool_checkY = Checking_empty(Y_input,Y_Required,Y_Must) ;
 if(bool_checkY){
     if(Number(Year_input) <= DateNow.getFullYear() && Number(Year_input) >= 1 )
-    return true ;
+    {
+        Y_Must.style.visibility = "hidden" ;
+        Y_input.style.borderColor = "none" ;
+      return true ;  
+    }
     else {
         Y_Must.style.visibility = "visible" ;
         Y_input.style.borderColor = "red" ;
